2a0bc612a28e61ebbbcaecc541ba0b83054372f6
kathyebel <kathyebel@me.com>
2018-05-18 11:59:23 -0400

    Added layouts for blogs and portfolios to include per page styles.

---
+++ app/controllers/concerns/default_content.rb
@@ -0,0 +1,12 @@
+module DefaultContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = 'Katherine Ebel | Portfolio'
+    @seo_keywords = 'Katherine Ebel Developer Portfolio'
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module DevcampPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.compile = true
   end
 end
