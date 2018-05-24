1c943665059ec522d42490cf035e6cced01310f7
kathyebel <kathyebel@me.com>
2018-05-24 11:27:32 -0400

    Persist reordering portfolio items to database.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
       sign_up: 'register'
   }
 
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs do
     member do
