f0a28e211150aac429f94672d6e8fd5a10be101d
Katherine Ebel <kathyebel@me.com>
2018-05-24 23:41:12 -0400

    Implement helper to parse urls in tweets to link to tweets.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,8 @@
 module PagesHelper
+  def twitter_parser tweet
+    regex = %r((?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])))
+    tweet.gsub(regex) do |url|
+      "<a href=#{url} target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
