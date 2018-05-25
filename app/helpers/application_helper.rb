d02bc3c7005ebf7f22c4ec976704e4d47f94a599
kathyebel <kathyebel@me.com>
2018-05-24 20:37:29 -0400

    refactor alerts to use application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,13 @@ module ApplicationHelper
   def active? path
     'active' if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    alert_generator alert if alert
+  end
+
+  def alert_generator(msg)
+    js add_gritter msg, title: 'Katherine Ebel\'s Portofolio', sticky: false
+  end
 end
